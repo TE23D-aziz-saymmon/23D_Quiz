308ffb5887300a1dc84fcc7899f22ff077ee8ce3
Saymmon Aziz <saymmon.aziz@elev.ga.ntig.se>
2024-09-09 09:18:55 +0200

    ay!!

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Micke";
+bool isAlive = name == "Micke";
+
+int hp = 100;
+
+hp = hp - 10;
+
+hp -= 10;
+
+hp = hp - 1;
+hp -= 1;
+
+hp--;
+
+hp = hp - +4;
+
+Console.WriteLine(hp);
+
+
+if (hp >= 0)
+{
+    Console.WriteLine("Still alive!");
+}
+
+Console.ReadLine();
