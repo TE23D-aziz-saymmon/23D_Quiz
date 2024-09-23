1d4ccca81c78271b3fea66123ec0ee0a2f0be9f0
Saymmon Aziz <saymmon.aziz@elev.ga.ntig.se>
2024-09-23 09:53:11 +0200

    Christianity

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,25 +1,75 @@
-﻿string name = "Micke";
-bool isAlive = name == "Micke";
+﻿using System.Collections.Concurrent;
+using Microsoft.VisualBasic;
 
-int hp = 100;
-
-hp = hp - 10;
-
-hp -= 10;
-
-hp = hp - 1;
-hp -= 1;
-
-hp--;
-
-hp = hp - +4;
-
-Console.WriteLine(hp);
+int score = 0;
+string answer1 = "";
+string answer2 = "";
+string answer3 = "";
+Console.WriteLine("Welcome to my quiz regarding Christianity!");
 
 
-if (hp >= 0)
+
+Console.WriteLine("Who was the first human that God created?");
+Console.WriteLine("a) Adam  b) Steve  c) Barney");
+answer1 = Console.ReadLine();
+answer1 = answer1.ToLower();
+if (answer1 == "a")
 {
-    Console.WriteLine("Still alive!");
+    score++;
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Incorrect!");
 }
 
+
+
+Console.WriteLine("What happened during the Great Schism in 1054?");
+Console.WriteLine("a) Martin Luther started reforming");
+Console.WriteLine("b) Christianity split into Roman Catholicism and Eastern Orthodoxy");
+Console.WriteLine("c) John Calvin the Great got excommunicated by the Pope");
+answer2 = Console.ReadLine();
+answer2 = answer2.ToLower();
+if (answer2 == "b")
+{
+    score++;
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Incorrect!");
+}
+
+
+
+Console.WriteLine("Who wrote the book of Revelation?");
+Console.WriteLine("a) Jesus Christ  b) St. Paul  c) St. John");
+answer3 = Console.ReadLine();
+answer3 = answer3.ToLower();
+if (answer3 == "c")
+{
+    score++;
+    Console.WriteLine("Correct!");
+}
+else
+{
+    Console.WriteLine("Incorrect!");
+}
+
+Console.WriteLine("You got {score} points.");
+if (score == 0)
+{
+    Console.WriteLine("My brother in Christ, you really do need a deeper depth in history!");
+}
+else if (score < 3)
+{
+    Console.WriteLine("Alright friend, I see that you have some knowledge.");
+}
+else
+{
+    Console.WriteLine("Great job! You answered everything correctly!");
+}
+
+Console.WriteLine("Press ENTER to end this quiz.");
 Console.ReadLine();
